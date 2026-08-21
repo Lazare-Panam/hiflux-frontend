d8cc5a69a34d05d2f1856f75ab7f7bd457884d0a
Lazare Kassi <sales@panamvalves.co.uk>
2026-08-21 16:14:26 +0100

    feat:added-hiflux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,18 @@ export default function RootLayout({
           type="text/javascript"
           src="https://cdn.seoplatform.io/injector.js?websiteId=28611"
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "y5wirsyo68");
+      `,
+          }}
+        />
       </head>
       <body>
         <QueryProvider>
